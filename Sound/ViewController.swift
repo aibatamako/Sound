23b4d1dd46a0490f62592df6897dcb6cfc43445c
Tamako Aiba <t.aiba@4nonome.com>
2021-05-10 09:44:50 +0900

    サウンドアプリチャレンジの完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -10,9 +10,11 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
     let drumSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name:"drumSound")!.data)
-
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "pianoSound")!.data)
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitarSound")!.data)
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -23,5 +25,24 @@ class ViewController: UIViewController {
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
     }
+    @IBAction func touchDownPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoPlayingImage"),for: .normal)
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+        
+        }
+    @IBAction func touchUpPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    @IBAction func touchDownGuitarButton(){
+        guitarButton.setImage(UIImage(named:"guitarPlayingImage"),for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+
+        }
+    @IBAction func touchUpGuitarButton(){
+            guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
+}
+    
 }
 
